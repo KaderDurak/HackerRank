3ef700eb59052f4f3e752c4cb85744ef78fa8e73
Kader Durak <59961853+KaderDurak@users.noreply.github.com>
2020-12-05 00:52:27 +0300

    Update Weather Observation Station 8.sql

--- SQL/Weather Observation Station 8.sql
+++ SQL/Weather Observation Station 8.sql
@@ -1,1 +1,5 @@
-select distinct city from station where regexp_like(city, '^[aeiou]+(.)+[aeiou]$', 'i') ;
+/*CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters.*/
+
+select distinct city 
+from station 
+where regexp_like(city, '^[aeiou]+(.)+[aeiou]$', 'i') ;
