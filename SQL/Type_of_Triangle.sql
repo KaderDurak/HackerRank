d7bfa11be21e59290b9b46eb8dc2444c1d36ff6e
Kader Durak <59961853+KaderDurak@users.noreply.github.com>
2020-12-08 15:49:05 +0300

    Update Type_of_Triangle.sql

--- SQL/Type_of_Triangle.sql
+++ SQL/Type_of_Triangle.sql
@@ -1,4 +1,4 @@
-Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
+/* Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
 
 Equilateral: It's a triangle with  sides of equal length.
 Isosceles: It's a triangle with  sides of equal length.
@@ -9,7 +9,7 @@ Not A Triangle: The given values of A, B, and C don't form a triangle.
 - Isosceles
 - Equilateral
 - Scalene
-- Not A Triangle
+- Not A Triangle */
 
 
 SELECT CASE WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
